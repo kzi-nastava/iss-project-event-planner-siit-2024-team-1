476b21049ad7df7ac4acae9a24d193fbc20d5985
MihajloCuic <158320399+MihajloCuic@users.noreply.github.com>
2024-11-25 16:03:08 +0100

    Added DTO for favorite merchandises

--- EventPlanner/src/main/java/com/example/eventplanner/dto/merchandise/FavoriteResponseDTO.java
+++ EventPlanner/src/main/java/com/example/eventplanner/dto/merchandise/FavoriteResponseDTO.java
@@ -1,4 +1,11 @@
 package com.example.eventplanner.dto.merchandise;
 
+import com.example.eventplanner.model.merchandise.Merchandise;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class FavoriteResponseDTO {
+    private List<Merchandise> favoriteMerchandises;
 }
