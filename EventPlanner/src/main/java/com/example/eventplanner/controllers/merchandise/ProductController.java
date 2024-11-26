5654b16d438f2d817fa19cc256e7ee777c25ac18
MihajloCuic <158320399+MihajloCuic@users.noreply.github.com>
2024-11-26 14:24:10 +0100

    added product details endpoint

--- EventPlanner/src/main/java/com/example/eventplanner/controllers/merchandise/ProductController.java
+++ EventPlanner/src/main/java/com/example/eventplanner/controllers/merchandise/ProductController.java
@@ -1,12 +1,17 @@
 package com.example.eventplanner.controllers.merchandise;
 
 import com.example.eventplanner.dto.merchandise.product.BuyProductResponseDTO;
+import com.example.eventplanner.dto.merchandise.product.GetProductByIdResponseDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/products")
 public class ProductController {
+    @GetMapping("/{id}")
+    public ResponseEntity<GetProductByIdResponseDTO> GetById(@PathVariable( value = "id") long id){
+        return ResponseEntity.ok(new GetProductByIdResponseDTO());
+    }
     @PostMapping("/{id}/buy")
     public ResponseEntity<com.example.eventplanner.dto.merchandise.product.BuyProductResponseDTO> buyProduct(@PathVariable( value = "id") int id) {
         return ResponseEntity.ok(new com.example.eventplanner.dto.merchandise.product.BuyProductResponseDTO());
