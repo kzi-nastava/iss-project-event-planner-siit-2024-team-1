eb176fbb4d70d751523fe389d5b7e188396cd06e
Nikson <nikolaking88@gmail.com>
2024-12-17 17:57:58 +0100

    Added photos controller

--- EventPlanner/src/main/java/com/example/eventplanner/controllers/photo/PhotoController.java
+++ EventPlanner/src/main/java/com/example/eventplanner/controllers/photo/PhotoController.java
@@ -4,14 +4,14 @@ import com.example.eventplanner.services.PhotoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.net.MalformedURLException;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1/photos")
 public class PhotoController {
@@ -26,4 +26,44 @@ public class PhotoController {
                 .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + resource.getFilename() + "\"")
                 .body(resource);
     }
+
+//    @PostMapping("/user")
+//    public ResponseEntity<Integer> uploadUserPhoto(@RequestParam("file") MultipartFile file) {
+//        int photoId = photoService.storeUserPhoto(file);
+//        return ResponseEntity.ok(photoId);
+//    }
+
+    @PostMapping("/business")
+    public ResponseEntity<Integer> uploadBusinessPhoto(@RequestParam("file") MultipartFile file) {
+        int photoId = photoService.storeBusinessPhoto(file);
+        return ResponseEntity.ok(photoId);
+    }
+
+    @PostMapping("/merchandise")
+    public ResponseEntity<Integer> uploadMerchandisePhoto(@RequestParam("file") MultipartFile file) {
+        int photoId = photoService.storeMerchandisePhoto(file);
+        return ResponseEntity.ok(photoId);
+    }
+
+    @RequestMapping(value = "/merchandise/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Integer> deleteMerchandisePhoto(@PathVariable int id) {
+        try {
+            photoService.deleteMercById(id);
+            return ResponseEntity.noContent().build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @RequestMapping(value = "/business/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Integer> deleteBusinessPhoto(@PathVariable int id) {
+        try {
+            photoService.deleteBusinessById(id);
+            return ResponseEntity.noContent().build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+
 }
--- EventPlanner/src/main/java/com/example/eventplanner/services/PhotoService.java
+++ EventPlanner/src/main/java/com/example/eventplanner/services/PhotoService.java
@@ -1,21 +1,126 @@
 package com.example.eventplanner.services;
 
+import com.example.eventplanner.model.merchandise.MerchandisePhoto;
+import com.example.eventplanner.model.user.BusinessPhoto;
+import com.example.eventplanner.model.user.ServiceProvider;
+import com.example.eventplanner.model.user.User;
+import com.example.eventplanner.repositories.merchandise.MerchandisePhotoRepository;
+import com.example.eventplanner.repositories.merchandise.MerchandiseRepository;
+import com.example.eventplanner.repositories.user.BusinessPhotoRepository;
+import com.example.eventplanner.repositories.user.ServiceProviderRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cglib.core.Local;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalTime;
 
 @Service
+@RequiredArgsConstructor
 public class PhotoService {
+    private final BusinessPhotoRepository businessPhotoRepository;
+    private final ServiceProviderRepository serviceProviderRepository;
+    private final MerchandiseRepository merchandiseRepository;
+    private final MerchandisePhotoRepository merchandisePhotoRepository;
     @Value("${photo.storage.path}") // Path to the folder where photos are stored
     private String photoStoragePath;
 
     public Resource getPhoto(String filename) throws MalformedURLException {
         return loadPhotoAsResource(filename);
+    }
+
+    public String storeUserPhoto(MultipartFile file, User user) {
+        try {
+            String originalFilename = file.getOriginalFilename();
+            String extension = originalFilename.substring(originalFilename.lastIndexOf('.'));
+            String baseName = originalFilename.substring(0, originalFilename.lastIndexOf('.'));
+            String filename = originalFilename;
+            Path path = Paths.get(photoStoragePath).resolve(filename);
+
+            // Check if file already exists
+            if (Files.exists(path)) {
+                String timestamp = LocalTime.now().toString().replace(":", "-").replace(".", "-");
+                filename = baseName + "-" + timestamp + extension;
+                path = Paths.get(photoStoragePath).resolve(filename);
+            }
+
+            Files.copy(file.getInputStream(), path);
+            user.setPhoto(filename); // Assuming User entity has a photo attribute to store the filename
+            return filename;
+        } catch (IOException e) {
+            throw new RuntimeException("Could not store file " + file.getOriginalFilename(), e);
+        }
+    }
+
+    public int storeBusinessPhoto(MultipartFile file) {
+        try {
+            String originalFilename = file.getOriginalFilename();
+            String extension = originalFilename.substring(originalFilename.lastIndexOf('.'));
+            String baseName = originalFilename.substring(0, originalFilename.lastIndexOf('.'));
+            String filename = originalFilename;
+            Path path = Paths.get(photoStoragePath).resolve(filename);
+
+            // Check if file already exists
+            if (Files.exists(path)) {
+                String timestamp = LocalTime.now().toString().replace(":", "-").replace(".", "-");
+                filename = baseName + "-" + timestamp + extension;
+                path = Paths.get(photoStoragePath).resolve(filename);
+            }
+
+            Files.copy(file.getInputStream(), path);
+            BusinessPhoto businessPhoto = new BusinessPhoto();
+            businessPhoto.setPhoto(filename);
+            BusinessPhoto saved = businessPhotoRepository.save(businessPhoto);
+            return saved.getId();
+        } catch (IOException e) {
+            throw new RuntimeException("Could not store file " + file.getOriginalFilename(), e);
+        }
+    }
+
+
+    public int storeMerchandisePhoto(MultipartFile file) {
+        try {
+            String originalFilename = file.getOriginalFilename();
+            String extension = originalFilename.substring(originalFilename.lastIndexOf('.'));
+            String baseName = originalFilename.substring(0, originalFilename.lastIndexOf('.'));
+            String filename = originalFilename;
+            Path path = Paths.get(photoStoragePath).resolve(filename);
+
+            // Check if file already exists
+            if (Files.exists(path)) {
+                String timestamp = LocalTime.now().toString().replace(":", "-").replace(".", "-");
+                filename = baseName + "-" + timestamp + extension;
+                path = Paths.get(photoStoragePath).resolve(filename);
+            }
+
+            Files.copy(file.getInputStream(), path);
+            MerchandisePhoto merchandisePhoto = new MerchandisePhoto();
+            merchandisePhoto.setPhoto(filename);
+            MerchandisePhoto saved = merchandisePhotoRepository.save(merchandisePhoto);
+            return saved.getId();
+        } catch (IOException e) {
+            throw new RuntimeException("Could not store file " + file.getOriginalFilename(), e);
+        }
+    }
+
+    public int deleteMercById(int id){
+        MerchandisePhoto merchandisePhoto = merchandisePhotoRepository.findById(id).orElseThrow();
+        merchandisePhotoRepository.delete(merchandisePhoto);
+        return merchandisePhoto.getId();
+    }
+
+    public int deleteBusinessById(int id){
+        BusinessPhoto businessPhoto = businessPhotoRepository.findById(id).orElseThrow();
+        businessPhotoRepository.delete(businessPhoto);
+        return businessPhoto.getId();
     }
 
     private Resource loadPhotoAsResource(String filename) throws MalformedURLException {
