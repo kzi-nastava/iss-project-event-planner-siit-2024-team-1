b10838acff101a94061f732181ecc85da5b506d7
Nikson <nikolaking88@gmail.com>
2025-01-17 20:41:52 +0100

    Minor changes to solve minor bugs

--- EventPlanner/src/main/java/com/example/eventplanner/config/SecurityConfig.java
+++ EventPlanner/src/main/java/com/example/eventplanner/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -37,32 +38,28 @@ public class SecurityConfig {
     }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-        return http
+        http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(
-                        req->req.requestMatchers("/api/v1/auth/login", "/api/v1/auth/refresh_token/**","/api/v1/events/top",
-                                        "api/v1/events/search","api/v1/merchandise/top","api/v1/services/search","api/v1/products/search",
-                                        "api/v1/events/{id}","api/v1/merchandise/{id}", "/api/v1/notifications/**", "api/v1/photos/{filename}")
-                                .permitAll()
-                                .anyRequest().permitAll()
-                                //.authenticated()
-                ).userDetailsService(userDetailsServiceImp)
-                .sessionManagement(session->session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                .exceptionHandling(
-                        e->e.accessDeniedHandler(
-                                        (request, response, accessDeniedException)->response.setStatus(403)
-                                )
-                                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
-                .logout(l->l
-                        .logoutUrl("/logout")
-                        .addLogoutHandler(logoutHandler)
-                        .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext()
-                        ))
-                .build();
+                .cors(Customizer.withDefaults()) // Add this line for CORS
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/api/v1/auth/login", "/api/v1/auth/refresh_token/**", "/api/v1/events/top/**", "/api/v1/auth/activate/**",
+                                "/api/events/{id}/details", "/api/v1/events/search", "/api/v1/merchandise/top/**", "/api/v1/auth/fast-register/**",
+                                "/api/v1/services/search", "/api/v1/products/search", "/api/v1/events/{id}",
+                                "/api/v1/merchandise/{id}", "/api/v1/notifications/**", "/api/v1/photos/{filename}", "/api/v1/photos/**", "/api/v1/photos/user/{id}",
+                                "/api/v1/auth/register-sp/**", "/api/v1/auth/register-eo/**" ,"/ws")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+                )
+                .exceptionHandling(ex -> ex
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                .sessionManagement(sess -> sess
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
+        return http.build();
     }
 //    @Bean
 //    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
