ead7d08b011b521c66fcbca64a75a16db54e4519
Nikson <nikolaking88@gmail.com>
2025-06-18 11:50:45 +0200

    Tested fixed auth features

--- EventPlanner/src/main/java/com/example/eventplanner/config/UserFactory.java
+++ EventPlanner/src/main/java/com/example/eventplanner/config/UserFactory.java
@@ -1,26 +1,34 @@
 package com.example.eventplanner.config;
 
+import com.example.eventplanner.model.auth.Role;
 import com.example.eventplanner.model.user.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 public class UserFactory {
 
     public static SecurityUser create(User user) {
         Collection<? extends GrantedAuthority> authorities;
+
         try {
-            authorities = AuthorityUtils.commaSeparatedStringToAuthorityList(user.getAuthorities());
+            Role role = user.getRole(); // koristi Enum
+            String roleName = role.name(); // npr. "EO", "AU", itd.
+
+            authorities = List.of(new SimpleGrantedAuthority("ROLE_" + roleName));
         } catch (Exception e) {
-            authorities = null;
+            authorities = List.of();
         }
 
         return new SecurityUser(
                 user.getId(),
                 user.getUsername(),
                 user.getPassword(),
-            authorities
+                authorities
         );
     }
 
--- EventPlanner/src/main/java/com/example/eventplanner/controllers/eventType/EventTypeController.java
+++ EventPlanner/src/main/java/com/example/eventplanner/controllers/eventType/EventTypeController.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
@@ -41,6 +42,7 @@ public class EventTypeController {
         return ResponseEntity.ok(eventTypeService.getAllActive());
     }
 
+    @PreAuthorize("hasAnyRole('SP', 'EO')")
     @GetMapping("/{id}")
     public ResponseEntity<EventTypeOverviewDTO> getById(@PathVariable int id
     ) {
